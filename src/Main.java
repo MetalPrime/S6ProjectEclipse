9c1b8284be341de16f11c7b95c343e9dcfba0a59
Jhon Garcia <metalprime57@gmail.com>
2020-09-28 19:28:01 -0500

    conexion secure

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import processing.core.PApplet;
 
 public class Main extends PApplet{
@@ -7,16 +17,74 @@ public class Main extends PApplet{
 		PApplet.main(Main.class.getName());
 	}
 	
+	private Socket socket;
+	private BufferedReader reader;
+	private BufferedWriter writer;
+	
 	public void settings() {
 		size(500, 500);
 	}
 	
 	public void setup() {
-		
+		initServer();
 	}
 	
 	public void draw() {
 		background(180);
 	}
+	
+	public void mouseClicked() {
+		sendMessage("Click desde servidor");
+	}
+	
+	public void initServer() {
+		new Thread(
+				()->{
+					try {
+						// 1. esperar la conexión
+						ServerSocket server = new ServerSocket(5000);
+						System.out.println("Esperando conexión");
+						socket = server.accept();
+						
+						// 3. conexión establecida
+						System.out.println("Cliente conectado");
+						InputStream  is = socket.getInputStream();
+						InputStreamReader isr = new InputStreamReader(is);
+						reader = new BufferedReader(isr);
+						
+						OutputStream os = socket.getOutputStream();
+                        OutputStreamWriter osw = new OutputStreamWriter(os);
+                        writer = new BufferedWriter(osw);
+						 
+						while(true) {
+							System.out.println("Esperando...");
+							//No se define hasta que el cliente manda un elemento
+							String line = reader.readLine();
+							System.out.println("Recibido");
+							System.out.println("Recibido" + line + '\n');
+						}
+					} catch (Exception e) {
+						// TODO: handle exception
+					}
+					
+				}
+				).start();
+	}
+	
+	private void sendMessage(String msg) {
+        new Thread(
+                () -> {
+                    try {
+                        writer.write(msg+"\n");
+                        writer.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+        ).start();
+    }
+	
+	
+	
 
 }
